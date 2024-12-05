836780b4c0319eb3e7f2b3e8dcca9bb65bf84a1a
Gerard <gqueraltferre@gmail.com>
2024-12-05 01:24:47 +0100

    day 1 - part 2

--- source/day1/LocationLists.hs
+++ source/day1/LocationLists.hs
@@ -7,6 +7,12 @@ computeDistanceRecursive acc _ [] = acc
 computeDistanceRecursive acc [] _ = acc
 computeDistanceRecursive acc (l:ls) (r:rs) = computeDistanceRecursive (acc + abs (l - r)) ls rs
 
+countOccurrences :: Int -> [Int] -> Int
+countOccurrences _ [] = 0
+countOccurrences x (y:ys)
+  | x == y    = 1 + countOccurrences x ys
+  | otherwise = countOccurrences x ys
+
 splitLists :: [String] -> ([Int], [Int])
 splitLists listOfPairs = unzip $ map ((\[l,r] -> (read l, read r)) . words) listOfPairs
 
@@ -19,7 +25,14 @@ computeDistance listOfPairs = sumOfDistances
     (lList, rList) = sortLists $ splitLists listOfPairs
     sumOfDistances = computeDistanceRecursive 0 lList rList
 
+computeSimilarity :: [String] -> Int
+computeSimilarity listOfPairs = sumOfSimilarities
+  where
+    (lList, rList) = splitLists listOfPairs
+    sumOfSimilarities = sum $ map (\x -> x * countOccurrences x rList) lList
+
 main :: IO ()
 main = do
   input <- readInputOfDay 1
   print $ "Part 1: " ++ show (computeDistance $ lines input)
+  print $ "Part 2: " ++ show (computeSimilarity $ lines input)
