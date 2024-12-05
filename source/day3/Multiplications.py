bbda4313b9812cbc278c9c7888e53c87183b0bf4
Gerard <gqueraltferre@gmail.com>
2024-12-05 21:13:43 +0100

    cleanup day 3 - part 1

--- source/day3/Multiplications.py
+++ source/day3/Multiplications.py
@@ -5,15 +5,19 @@ from functools import reduce
 
 def find_valid_mul_commands(input: list[str]) -> list[str]:
     # using this regex we'll already get a list of tuples
-    regex = "mul\((?P<first>\d*),(?P<second>\d*)\)"
+    regex = re.compile(r"mul\((?P<first>\d*),(?P<second>\d*)\)")
 
     asSingleString = ''.join(input.readlines())
-    return re.findall(regex, asSingleString)
+    return regex.findall(asSingleString)
 
 
 def run_mul_commands(input: list[str]):
     valid_commands = find_valid_mul_commands(input)
-    return reduce((lambda acc, pair: acc + int(pair[0]) * int(pair[1])), valid_commands, 0)
+    acc = 0
+    for command in valid_commands:
+        first, second = command
+        acc += int(first) * int(second)
+    return acc
 
 
 if __name__ == "__main__":
