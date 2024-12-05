28355e289665c6fddae63f91416cb3166c0cf394
Gerard <gqueraltferre@gmail.com>
2024-12-05 19:31:48 +0100

    day 2 - part 2

--- source/day2/Reports.kt
+++ source/day2/Reports.kt
@@ -6,35 +6,108 @@ import kotlin.math.abs
 fun main() {
     val input = KotlinFileReader.readInputOfDay(2)
 
-    val safeReports = input.useLines {
+    val safeReportsPart1 = input.useLines {
         it.count { report ->
-            isReportSafe(report)
+            isReportSafe(rawReportToList(report))
         }
     }
+    println("Part 1: $safeReportsPart1")
 
-    println("Part 1: $safeReports")
+    val safeReportsPart2 = input.useLines {
+        it.count { report ->
+            isReportSafeWithDampener(rawReportToList(report))
+        }
+    }
+    println("Part 2 (honest attempt): $safeReportsPart2")
+
+    val safeReportsPart2BruteForce = input.useLines {
+        it.count { report ->
+            val reportAsList = rawReportToList(report)
+
+            if (isReportSafe(reportAsList)) {
+                return@count true
+            }
+            reportAsList.forEachIndexed { i, _ ->
+                if (isReportSafe(reportAsList.filterIndexed { j, _ -> i != j })) {
+                    return@count true
+                }
+            }
+            false
+        }
+    }
+    println("Part 2 (brute force): $safeReportsPart2BruteForce")
 }
 
-fun isReportSafe(i_report: String): Boolean {
-    val values = i_report.split(" ").map { it.toInt() }
+fun isPairSafe(i_leftValue: Int, i_rightValue: Int, i_increasing: Boolean): Boolean {
+// check distance
+    if (abs(i_leftValue - i_rightValue) > 3) return false
 
+    // check consistency in increasing or decreasing pattern
+    if (i_leftValue == i_rightValue) return false
+
+    val pairIsIncreasing = i_leftValue < i_rightValue
+    if (i_increasing && !pairIsIncreasing) return false
+    if (!i_increasing && pairIsIncreasing) return false
+
+    return true
+}
+
+fun rawReportToList(i_report: String): List<Int> {
+    return i_report.split(" ").map { it.toInt() }
+}
+
+fun isReportSafe(i_report: List<Int>): Boolean {
     var increasing: Boolean? = null
 
-    return values.zipWithNext().all { (previous, current) ->
-        // check distance
-        if (abs(previous - current) > 3) return@all false
-
-        // check consistency in increasing or decreasing pattern
-        if (previous == current) return@all false
-
-        val pairIsIncreasing = previous < current
+    return i_report.zipWithNext().all { (previous, current) ->
         if (increasing == null) {
             // initialize flag
-            increasing = pairIsIncreasing
+            increasing = previous < current
         }
-        if (increasing!! && !pairIsIncreasing) return@all false
-        if (!increasing!! && pairIsIncreasing) return@all false
-
-        true
+        isPairSafe(previous, current, increasing!!)
     }
+}
+
+fun isReportSafeWithDampener(i_report: List<Int>): Boolean {
+    var increasing: Boolean? = null
+    var dampenedError = false
+
+    var i = 0
+    while (i < i_report.size - 1) {
+        val previous = i_report[i]
+        val current = i_report[i + 1]
+
+        if (isPairSafe(previous, current, increasing ?: (previous < current))) {
+            increasing = increasing ?: (previous < current)
+            ++i
+            continue
+        }
+
+        if (dampenedError) {
+            // already used the dampener!
+            return false
+        }
+
+        dampenedError = true
+        if (i == 0) {
+            // special case, failed on the first iteration
+            // skip it and try again
+            ++i
+            continue
+        }
+
+        if (i + 2 == i_report.size) {
+            // removing the last value fixes it!
+            // return already, nothing to do
+            return true
+        }
+        if (!isPairSafe(current, i_report[i + 2], increasing!!)) {
+            // couldn't dampen error
+            return false
+        }
+        // skip next iteration
+        i += 2
+    }
+
+    return true
 }
